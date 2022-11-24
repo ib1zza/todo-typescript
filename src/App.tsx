615776348766a4e72c292c517b162731a03816cc
ib1zza <dremast1337@gmail.com>
2022-11-24 09:06:18 +0300

    added redux-persist for localstorage data sync

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ import "./nullstyle.css";
 import HomePage from "./pages/HomePage";
 import Navigation from "./components/Navigation";
 import CompletedPage from "./pages/CompletedPage";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/store";
 
 function App() {
   return (
     <div className={"App"}>
       <Navigation />
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/completed" element={<CompletedPage />} />
-      </Routes>
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+
+          <Route path="/completed" element={<CompletedPage />} />
+        </Routes>
+      </PersistGate>
     </div>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,41 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import TodoSlice from "./reducers/TodoSlice";
 import TodoCompletedSlice from "./reducers/TodoCompletedSlice";
 
+const rootReducer = combineReducers({
+  todo: TodoSlice,
+  todoCompleted: TodoCompletedSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: { todo: TodoSlice, todoCompleted: TodoCompletedSlice },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
